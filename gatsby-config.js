0aa40b2d39b5577ccf9ba38c3dfb479aa81690dd
dinomoon <ansrud45@gmail.com>
2021-01-29 19:47:23 +0900

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `💾 MoonK`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
