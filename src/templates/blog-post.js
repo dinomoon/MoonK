8316918f37e1363d38012042aa4a499be8499015
dinomoon <ansrud45@gmail.com>
2021-01-30 17:34:33 +0900

    utteranc 댓글 적용

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Utterances from "../components/Utterances"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -60,6 +61,7 @@ const BlogPostTemplate = ({ data, location }) => {
           </li>
         </ul>
       </nav>
+      <Utterances />
     </Layout>
   )
 }
